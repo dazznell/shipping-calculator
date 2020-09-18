a03ef6dc6d433a307a67a182ed3fb64249e846ab
dazznell <56004605+dazznell@users.noreply.github.com>
2020-09-17 22:32:09 -0600

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,24 @@
+// single line comment
+
+/*  alt + shift + a  | multi-line comment */
+
+/* 
+
+    DOM | Document Object Model 
+
+    methods something the language does for you
+
+    method()
+    getElementById():
+
+    variable declaration
+
+    statement
+    object.   method()   -- work
+    document  querySelector()
+
+    let - change data type
+    const - will not change
+
+*/
+
